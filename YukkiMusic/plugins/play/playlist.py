909ebb879ef71b138bbe6c46b035afe6d9bb3d72
Team-techno <103729598+Team-techno@users.noreply.github.com>
2022-05-11 13:02:25 +0530

    Update playlist.py

--- YukkiMusic/plugins/play/playlist.py
+++ YukkiMusic/plugins/play/playlist.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 from random import randint
 
@@ -17,7 +8,7 @@ from pyrogram.types import (InlineKeyboardButton,
 
 from config import BANNED_USERS, SERVER_PLAYLIST_LIMIT
 from strings import get_command
-from YukkiMusic import Carbon, YouTube, app
+from YukkiMusic import YouTube, app
 from YukkiMusic.utils.database import (delete_playlist, get_playlist,
                                        get_playlist_names,
                                        save_playlist)
