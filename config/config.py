403c327c0c7d6e637d3328c3f0fe7c4cedbf5b94
Team-techno <103729598+Team-techno@users.noreply.github.com>
2022-06-29 13:50:35 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Team-techno/Abcdmusic",
+    "https://github.com/Team-techno/Abcdmus",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
